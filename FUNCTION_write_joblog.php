0d233d477c15ffe3eb726b375d3ea988c4af43ef
Roland Smith <rolandksmith@gmail.com>
2025-02-02 16:08:29 -0700

    Changed to call write_joblog2_func. This will be used until I fix all programs

--- FUNCTION_write_joblog.php
+++ FUNCTION_write_joblog.php
@@ -3,11 +3,12 @@ function write_joblog_func($dataToWrite="",$doDebug=FALSE) {
 /* Input data:
 	job name|date (y-m-d)|time (h:i:s)|who|mode|data type|additional info|ip|comments
 	
-	Function writes the dataToWrite the database
+	Function formats the data to call write_joblog2_func
 	
 	returns:
-		array('SUCCESS','Write was successful'	if OK
-		array('FALSE',wpdb last error			if failed
+		TRUE if successful
+		FALSE if not successful
+
 */
 	global $wpdb;
 //	$doDebug				= FALSE;
@@ -20,10 +21,11 @@ function write_joblog_func($dataToWrite="",$doDebug=FALSE) {
 	$job_data_type			= "";
 	$job_addl_info			= "";
 	$job_ip_addr			= "";
+	$job_month				= "";
 	$job_comments			= "";
+	$job_title				= "";
+
 	$updateParams			= array();
-	$updateFormat			= array();
-	$joblogTableName		= "wpw1_cwa_joblog";
 
 	if ($doDebug) {
 		echo "<br />FUNCTION write_job_log called with dataToWrite: $dataToWrite<br />";
@@ -32,71 +34,45 @@ function write_joblog_func($dataToWrite="",$doDebug=FALSE) {
 
 	$initializationArray				= data_initialization_func();
 	$myArray							= explode("|",$dataToWrite);
-	$updateParams['job_name']			= $myArray[0];
-	$updateParams['job_date']			= $myArray[1];
-	$updateParams['job_time']			= $myArray[2];
-	$updateParams['job_who']			= $myArray[3];
-	$updateParams['job_mode']			= $myArray[4];
-	$updateParams['job_data_type']		= $myArray[5];
-	$updateParams['job_addl_info']		= $myArray[6];
-
-	if (array_key_exists(7,$myArray)) {
-		$updateParams['job_ip_addr']	= $myArray[7];
-	} else {
-		$ipAddr							= get_the_user_ip();
-		$updateParams['job_ip_addr']	= $ipAddr;
-	}
-
-	if (array_key_exists(8,$myArray)) {
-		$updateParams['job_comments']	= $myArrray[8];
-	} else {
-		$updateParams['job_comments']	= 'None supplied';
-	}
-	
-	$checkJobname			= $myArray[0];
-	$badJobname				= FALSE;
-	if (preg_match('/jobname/',$checkJobname)) {
-		$badJobname			= TRUE;
-	}
-	if (preg_match('/FIX/',$checkJobname)) {
-		$badJobname			= TRUE;
-	}
-	if ($checkJobname == '') {
-		$badJobname			= TRUE;
-	}
-	if ($badJobname) {
-		$variableDump	= get_defined_vars();
-		$newStr			= print_r($variableDump,TRUE);
-		$myStr			= the_title();
-		sendErrorEmail("function_write_joblog: jobname. $myStr\n<br /><pre>$newStr</pre>");
-	}
-	
-	
-	$updateParams['job_month']			= date('F Y');
-	$updateFormat						= array('%s','%s','%s','%s','%s','%s','%s','%s','%s');
-
-	$result							= $wpdb->insert($joblogTableName,
-													$updateParams,
-													$updateFormat);
-	if ($result === FALSE) {
-		if ($doDebug) {
-			echo "Inserting $dataToWrite failed<br />
-				  wpdb->last_query: " . $wpdb->last_query . "<br />
-				  <b>wpdb->last_error: " . $wpdb->last_error . "</b><br />";
+	$thisII								= 0;
+	for ($thisII= 0;$thisII < count($myArray);$thisII++) {
+		switch($thisII) {
+			case 0:
+				$updateParams['jobname']			= $myArray[0];
+				break;
+			case 1:
+				$updateParams['jobdate']			= $myArray[1];
+				break;
+			case 2:
+				$updateParams['jobtime']			= $myArray[2];
+				break;
+			case 3:
+				$updateParams['jobwho']				= $myArray[3];
+			case 4:
+				$updateParams['jobmode']			= $myArray[4];
+				break;
+			case 5:
+				$updateParams['jobdatatype']		= $myArray[5];
+				break;
+			case 6:
+				$updateParams['jobaddlinfo']		= $myArray[6];
+				break;
+			case 7:
+				$updateParams['jobip']				= $myArray[7];
+				break;
+			case 8:
+				$updateParams['jobcomments']		= $myArray[8];
+				break;
 		}
-		$myStr				= $wpdb->last_error;
-		return array('FALSE',$myStr);
-	} else {
-		if ($doDebug) {
-			$insertID			= $wpdb->insert_id;
-			echo "write to $joblogTableName was successful<br />
-					id $insertID was inserted<br />
-					updateParams:<br /><pre>";
-			print_r($updateParams);
-			echo "</pre><br />";
-		}
-		return array('SUCCESS','Write was successful');
 	}
-
+	$updateParams['doDebug']						= $doDebug;
+	
+	if ($doDebug) {
+		echo "passing these parameters to write_joblog2:<br /><pre>";
+		print_r($updateParams);
+		echo "</pre><br />";
+	}
+	$result		= write_joblog2_func($updateParams);
+	return $result;
 }
 add_action('write_joblog_func','write_joblog_func');
