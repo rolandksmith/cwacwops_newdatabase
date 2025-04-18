4faa67cef92811003cc40517811c329383b95204
Roland Smith <rolandksmith@gmail.com>
2025-04-18 12:56:08 -0600

    Corrected spelling of siteurl

--- daily_temp_data_cleanup.php
+++ daily_temp_data_cleanup.php
@@ -34,7 +34,7 @@ function daily_temp_data_cleanup_func() {
 		echo "</pre><br />";
 	}
 	$userName				= $initializationArray['userName'];
-	$siteURL				= $initializationArray['siteURL'];
+	$siteURL				= $initializationArray['siteurl'];
 	ini_set('display_errors','1');
 	error_reporting(E_ALL);	
 	ini_set('memory_limit','256M');
