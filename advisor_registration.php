86472a4ffaadcd61091a7824dbba5b10b6d75462
Roland Smith <rolandksmith@gmail.com>
2025-12-06 20:00:34 -0700

    added language field

--- advisor_registration.php
+++ advisor_registration.php
@@ -1480,7 +1480,7 @@ function getAdvisorInfoToDisplay($inp_callsign,$inp_semester,$noUpdate) {
 						}
 						if ($firstTime) {
 							$firstTime			= FALSE;
-							$languageOptions		.= "<input type='radio' class='formInputButton' name='inp_advisorclass_language' value='$thisLanguage $languageChecked' >$thisLanguage";
+							$languageOptions		.= "<input type='radio' class='formInputButton' name='inp_advisorclass_language' value='$thisLanguage' $languageChecked' >$thisLanguage";
 						} else {
 							$languageOptions		.= "<br /><input type='radio' class='formInputButton' name='inp_advisorclass_language' value='$thisLanguage' $languageChecked >$thisLanguage";
 						}
