86e069ad974c164bf610a499cd8365411e59c006
Roland Smith <rolandksmith@gmail.com>
2026-01-20 04:28:24 -0700

    added data_log to tables to be updated

--- update_callsign.php
+++ update_callsign.php
@@ -23,7 +23,7 @@ function update_callsign_func() {
 	$userName			= $initializationArray['userName'];
 	
 	if ($userName == '') {
-		$content		.= "You are not authorized";
+		$content		= "You are not authorized";
 		return $content;
 	}
 	
@@ -108,66 +108,7 @@ function update_callsign_func() {
 	}
 	
 	
-	$content = "<style type='text/css'>
-		fieldset {font:'Times New Roman', sans-serif;color:#666;background-image:none;
-		background:#efefef;padding:2px;border:solid 1px #d3dd3;}
-
-		legend {font:'Times New Roman', sans-serif;color:#666;font-weight:bold;
-		font-variant:small-caps;background:#d3d3d3;padding:2px 6px;margin-bottom:8px;}
-
-		label {font:'Times New Roman', sans-serif;font-weight:bold;line-height:normal;
-		text-align:right;margin-right:10px;position:relative;display:block;float:left;width:150px;}
-
-		textarea.formInputText {font:'Times New Roman', sans-serif;color:#666;
-		background:#fee;padding:2px;border:solid 1px #f66;margin-right:5px;margin-bottom:5px;}
-
-		textarea.formInputText:focus {color:#000;background:#ffffff;border:solid 1px #006600;}
-
-		textarea.formInputText:hover {color:#000;background:#ffffff;border:solid 1px #006600;}
-
-		input.formInputText {color:#666;background:#fee;padding:2px;
-		border:solid 1px #f66;margin-right:5px;margin-bottom:5px;}
-
-		input.formInputText:focus {color:#000;background:#ffffff;border:solid 1px #006600;}
-
-		input.formInputText:hover {color:#000;background:#ffffff;border:solid 1px #006600;}
-
-		input.formInputFile {color:#666;background:#fee;padding:2px;border:
-		solid 1px #f66;margin-right:5px;margin-bottom:5px;height:20px;}
-
-		input.formInputFile:focus {color:#000;background:#ffffff;border:solid 1px #006600;}
-
-		select.formSelect {color:#666;background:#fee;padding:2px;
-		border:solid 1px #f66;margin-right:5px;margin-bottom:5px;cursor:pointer;}
-
-		select.formSelect:hover {color:#333;background:#ccffff;border:solid 1px #006600;}
-
-		input.formInputButton {vertical-align:middle;font-weight:bolder;
-		text-align:center;color:#300;background:#f99;padding:1px;border:solid 1px #f66;
-		cursor:pointer;position:relative;float:left;}
-
-		input.formInputButton:hover {color:#f8f400;}
-
-		input.formInputButton:active {color:#00ffff;}
-
-		tr {color:#333;background:#eee;}
-
-		table{font:'Times New Roman', sans-serif;background-image:none;border-collapse:collapse;}
-
-		th {color:#ffff;background-color:#000;padding:5px;font-size:small;}
-
-		td {padding:5px;font-size:small;}
-
-		th:first-child,
-		td:first-child {
-		 padding-left: 10px;
-		}
-
-		th:last-child,
-		td:last-child {
-			padding-right: 5px;
-		}
-		</style>";	
+	$content = "";
 
 	if ($testMode) {
 		$content	.= "<p><strong>Operating in Test Mode.</strong></p>";
@@ -218,7 +159,8 @@ function update_callsign_func() {
 								'wpw1_cwa_user_master_history|historywho&historycallsign||record_id',
 								'wpw1_cwa_user_master|user_call_sign|user_action_log|user_ID',
 								'wpw1_users|user_login&user_nicename||ID',
-								'wpw1_cwa_faq|faq_submitted_by||faq_record_id');
+								'wpw1_cwa_faq|faq_submitted_by||faq_record_id',
+								'wpw1_cwa_data_log|data_call_sign||data_record_id');
 		$userMasterTableName	= 'wpw1_cwa_user_master';
 		$studentTableName		= 'wpw1_cwa_student';
 		$advisorClassTableName	= 'wpw1_cwa_advisorclass';
