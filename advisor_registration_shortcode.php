50df938091ceee2651fdedd65ea8ce1d61f10b77
Roland Smith <rolandksmith@gmail.com>
2026-02-02 09:46:09 -0700

    New Program

---
+++ advisor_registration_shortcode.php
@@ -0,0 +1,7 @@
+ * WordPress shortcode registration
+ */
+function advisor_registration_func() {
+    $registration = new CWA_Advisor_Registration();
+    return $registration->handle();
+}
+add_shortcode('advisor_registration', 'advisor_registration_func');
